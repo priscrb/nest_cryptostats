9b4ee44c99ef4f3dd11a92ee6628f5a786e72630
Priscilla Ribeiro <prsc.ribeiro@gmail.com>
2022-01-23 23:01:17 -0300

    intial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user-request.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(
+    @Body() createUserRequest: CreateUserRequest,
+  ): Promise<void> {
+    return this.usersService.createUser(createUserRequest);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user-request.dto';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly userRepository: UsersRepository) {}
+
+  async createUser(createUserRequest: CreateUserRequest): Promise<any> {
+    return this.userRepository.insertOne(createUserRequest);
+  }
+}
